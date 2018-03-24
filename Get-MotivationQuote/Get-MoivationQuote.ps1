431cb0736b864e1b648d7bd36a0556df9dd74ff7
Kamil Procyszyn <kprocyszyn@gmail.com>
2018-03-24 15:40:22 +0000

    aa

--- Get-MotivationQuote/Get-MoivationQuote.ps1
+++ Get-MotivationQuote/Get-MoivationQuote.ps1
@@ -6,26 +6,41 @@
 3. USe DLL to fetch the quote
 4. Would be nice to make it work with PS 6.0
 #>
-[CmdletBinding]
-$PSScriptRoot
+#[CmdletBinding]
 
 $DriverFile = Join-Path -Path (Get-Location).Path -ChildPath file.txt
 
 Test-Path $DriverFile
 
-Function Get-ChromeDriver {
 
-    $url = 'https://chromedriver.storage.googleapis.com/2.37/chromedriver_win32.zip'
+Function Get-WebDriver {
+
+    $url = 'https://goo.gl/aa746n'
+    $output = "$PSScriptRoot\webdriver.zip"
+    
+    Invoke-WebRequest -Uri $url -OutFile $output
+    Expand-Archive -Path $output -DestinationPath $PSScriptRoot -Force
+    Remove-Item $output
+
+    Install-Package "$PSScriptRoot\dist\Selenium.WebDriver.3.9.1.nupkg" -Verbose
+    }
+function Get-ChromeDriver {
+
+$url = 'https://chromedriver.storage.googleapis.com/2.37/chromedriver_win32.zip'
 $output = "$PSScriptRoot\chromedriver.zip"
-$start_time = Get-Date 
 
 Invoke-WebRequest -Uri $url -OutFile $output
-Expand-Archive -Path $output -DestinationPath $PSScriptRoot
+Expand-Archive -Path $output -DestinationPath $PSScriptRoot -Force
 Remove-Item $output
 }
 
-If (-not(Test-Path -Path $PSScriptRoot\chromedriver.exe)) {Get-ChromeDriver}
-
 function Remove-ChromeDriver {
     Remove-Item "$PSScriptRoot\chromedriver.exe"
+    Remove-Item "$PSScriptRoot\dist*" -Recurse
 } 
+
+Remove-ChromeDriver
+
+If (-not(Test-Path -Path $PSScriptRoot\chromedriver.exe)) {Get-ChromeDriver}
+
+Get-WebDriver -Verbose
