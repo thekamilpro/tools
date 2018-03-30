eb01107ee4da5d6d4b5e53109959861ff16f0f9b
kamil <kamil@company.pri>
2018-03-30 19:59:31 +0100

    0.1

---
+++ Get-ComputerInfo/Get-ComputerInfo.ps1
@@ -0,0 +1,33 @@
+#ComputerSystem
+$Hostname = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object Name
+$Manufacturer = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object Manufacturer
+$Model = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object Model
+#OperatingSystem
+$SystemDrive = Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object SystemDrive
+$SystemDrive = $SystemDrive.SystemDrive
+$SystemDrive = Get-CimInstance -ClassName Win32_LogicalDisk -filter "Name like '$SystemDrive'"
+$BuildNumber = Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object BuildNumber
+$BootTime = Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object LastBootUpTime
+$Version = Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object Version
+#Processpr
+$CPUName = Get-CimInstance -ClassName Win32_Processor |  Select-Object Name
+$CPUID = Get-CimInstance -ClassName Win32_Processor |  Select-Object DeviceID
+$CPUCores = Get-CimInstance -ClassName Win32_Processor |  Select-Object NumberOfCores
+#IP
+$IP = Get-NetIPAddress -AddressFamily IPv4 | Select-Object IPv4Address
+
+$Results = New-Object -TypeName psobject -Property @{
+    Hostname = $Hostname.Name
+    Manufacturer = $Manufacturer.Manufacturer
+    Model = $Model.Model
+    SystemDrive = $SystemDrive
+    BuildNumber = $BuildNumber.BuildNumber
+    BootTime = $BootTime.LastBootUpTime
+    Version = $Version.Version
+    CPUName = $CPUName.Name
+    CPUID = $CPUID.DeviceID
+    CPUCores = $CPUCores.NumberOfCores
+    IP = $IP
+}
+
+$Results | Select-Object Hostname,Manufacturer,Model,SystemDrive,BuildNumber,BootTime,Version,CPUName,CPUID,CPUCores,IP | Format-List
