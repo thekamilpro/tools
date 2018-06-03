dab81a103b3072a67a77d04dd8579e82f92f9104
Kamil Procyszyn <kprocyszyn@gmail.com>
2018-06-03 21:28:23 +0100

    Added template

---
+++ _Template.ps1
@@ -0,0 +1,18 @@
+function Template {
+    [CmdletBinding()]
+
+    Param (
+        [String] $Value
+    ) #Param
+
+    BEGIN {} #BEGIN
+
+    PROCESS {
+        Write-Verbose "Starting function"
+
+    } #PROCESS
+
+    END {} #END
+
+
+}#function
