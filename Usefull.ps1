e417d07339c06085c999c6d5f89124cc56f40e1b
Kamil Procyszyn <kprocyszyn@gmail.com>
2018-09-15 11:07:35 +0100

    Added useful

---
+++ Usefull.ps1
@@ -0,0 +1,2 @@
+#Replicate all domain contollers
+(Get-ADDomainController -Filter *).Name | Foreach-Object {repadmin /syncall $_ (Get-ADDomain).DistinguishedName /e /A | Out-Null}; Start-Sleep 10; Get-ADReplicationPartnerMetadata -Target "$env:userdnsdomain" -Scope Domain | Select-Object Server, LastReplicationSuccess
